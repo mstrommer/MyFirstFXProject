14ffed468067bffd48d52d3d7cb60917811ac459
Michael Strommer <69633126+mstrommer@users.noreply.github.com>
2024-12-12 16:33:25 +0100

    Removed World!

--- src/main/java/at/ac/fhcampuswien/myfirstfxproject/HelloApplication.java
+++ src/main/java/at/ac/fhcampuswien/myfirstfxproject/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         // TODO
-        System.out.println("Hello World!");
+        System.out.println("Hello");
     }
 
     public static void main(String[] args) {
